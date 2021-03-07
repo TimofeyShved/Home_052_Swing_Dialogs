617553ec93cfa7d436f94fc0992249cb98e5a344
timofeya123 <timofeya123@inbox.ru>
2021-03-07 10:16:23 +0600

    Dialogs

---
+++ src/com/Swing/Main.java
@@ -0,0 +1,67 @@
+package com.Swing;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    static JFrame jFrame = getFrame(); // наша форма
+    static JPanel jPanel = new JPanel(); // наша панель
+
+    public static void main(String[] args) {
+        // создаём панель с кнопкой, для вызова сообщений
+	    jFrame.add(jPanel);
+
+	    JButton jButton = new JButton("Ошибка");
+	    jPanel.add(jButton);
+	    // добавляем действие на кнопку
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(jPanel, "Сообщение об ошибке №-", "заголовок", JOptionPane.ERROR_MESSAGE); // сообщение об ошибке
+            }
+        });
+
+        JButton jButton2 = new JButton("Да или нет");
+        jPanel.add(jButton2);
+        // добавляем действие на кнопку
+        jButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(JOptionPane.showConfirmDialog(jPanel, "Сделать выбор, да или нет?", "заголовок", JOptionPane.OK_CANCEL_OPTION)); // 0 - нет / 2 - да
+            }
+        });
+
+        JButton jButton3 = new JButton("Множественный выбор");
+        jPanel.add(jButton3);
+        // добавляем действие на кнопку
+        jButton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(JOptionPane.showOptionDialog(jPanel, "выберете число:", "заголовок", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"1","2","3","4"
+                }, "2"));// 0 - нет / 2 - да
+            }
+        });
+
+        JButton jButton4 = new JButton("Ввод данных");
+        jPanel.add(jButton4);
+        // добавляем действие на кнопку
+        jButton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(JOptionPane.showInputDialog(jPanel, "Введите слово"));
+            }
+        });
+
+        jPanel.revalidate();
+    }
+
+    static JFrame getFrame(){
+        JFrame jFrame = new JFrame(); // создается форма
+        jFrame.setVisible(true); // влючаем видимость
+        jFrame.setBounds(750,250,450,200); // размеры
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // выход
+        return jFrame;
+    }
+}
